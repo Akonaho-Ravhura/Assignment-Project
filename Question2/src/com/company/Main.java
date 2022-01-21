19ec4c5464618130ebdb3fd6501f7b1fe896edba
Akonaho Ravhura <70781853+Akonaho-Ravhura@users.noreply.github.com>
2022-01-21 15:56:20 +0200

    Add files via upload

---
+++ Question2/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+import javax.swing.*;
+import java.util.*;
+public class Main {
+
+    String employeeName;
+    int init = 0;
+    int MTN = 83;
+    int vodacom = 71;
+    int cell_C = 84;
+
+    public static void main(String[] args) {
+        JOptionPane.showInputDialog("Please enter three employee");
+
+    }
+}
